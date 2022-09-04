fecb2a7888770da548a92727fb32d923b7238f96
jialewang1 <108407828+jialewang1@users.noreply.github.com>
2022-09-04 22:27:00 +0800

    Update networks.py

--- networks.py
+++ networks.py
@@ -13,75 +13,7 @@ except ImportError: # will be 3.x series
 
 ##################################################################################
 # Discriminator
-##################################################################################
-def gaussian_weights_init(m):
-  classname = m.__class__.__name__
-  if classname.find('Conv') != -1 and classname.find('Conv') == 0:
-    m.weight.data.normal_(0.0, 0.02)
-class LeakyReLUConv2d(nn.Module):
-  def __init__(self, n_in, n_out, kernel_size, stride, padding=0, norm='None', sn=False):
-    super(LeakyReLUConv2d, self).__init__()
-    model = []
-    model += [nn.ReflectionPad2d(padding)]
-    model += [nn.Conv2d(n_in, n_out, kernel_size=kernel_size, stride=stride, padding=0, bias=True)]
-    if 'norm' == 'Instance':
-      model += [nn.InstanceNorm2d(n_out, affine=False)]
-    model += [nn.LeakyReLU(inplace=True)]
-    self.model = nn.Sequential(*model)
-    self.model.apply(gaussian_weights_init)
-
-  def forward(self, x):
-    return self.model(x)
-
-class Dis_content(nn.Module):
-  def __init__(self):
-    super(Dis_content, self).__init__()
-    model = []
-    model += [LeakyReLUConv2d(256, 256, kernel_size=7, stride=2, padding=1, norm='Instance')]
-    model += [LeakyReLUConv2d(256, 256, kernel_size=7, stride=2, padding=1, norm='Instance')]
-    # model += [LeakyReLUConv2d(256, 256, kernel_size=7, stride=2, padding=1, norm='Instance')]
-    model += [LeakyReLUConv2d(256, 256, kernel_size=4, stride=1, padding=0)]
-    model += [nn.Conv2d(256, 1, kernel_size=1, stride=1, padding=0)]
-    self.model = nn.Sequential(*model)
-
-  def forward(self, x):
-    outs = self.model(x)
-    # results = []
-    # for out in outs:
-    #     out = out.view(-1)
-    #     results.append(out)
-    # outs.append(out)
-    return outs
-  def calc_dis_loss(self, input_fake, input_real):
-    pred_fake = self.forward(input_fake.detach())
-    pred_real = self.forward(input_real.detach())
-    loss_D = 0
-    for it, (out_a, out_b) in enumerate(zip(pred_fake, pred_real)):
-      out_fake = nn.functional.sigmoid(out_a)
-      out_real = nn.functional.sigmoid(out_b)
-      all1 = torch.ones((out_real.size())).cuda()
-      all0 = torch.zeros((out_fake.size())).cuda()
-      ad_true_loss = nn.functional.binary_cross_entropy(out_real, all1)
-      ad_fake_loss = nn.functional.binary_cross_entropy(out_fake, all0)
-      loss_D += (ad_true_loss + ad_fake_loss)
-    return loss_D/input_fake.shape[0]
-  
-  def calc_gen_loss(self, input_fake,input_real):
-    out1 = self.forward(input_fake)
-    out2 = self.forward(input_real)
-    loss_Adv=0
-    for out in out1:
-      outputs_fake = nn.functional.sigmoid(out)
-      all_half = 0.5*torch.ones((outputs_fake.size())).cuda()
-      ad_loss1 = nn.functional.binary_cross_entropy(outputs_fake, all_half)
-      loss_Adv+=ad_loss1
-    for out in out2:
-      outputs_fake = nn.functional.sigmoid(out)
-      all_half = 0.5*torch.ones((outputs_fake.size())).cuda()
-      ad_loss2 = nn.functional.binary_cross_entropy(outputs_fake, all_half)
-      loss_Adv+=ad_loss2
-    return loss_Adv/input_fake.shape[0]
-
+#################################################################################
 
 class MsImageDis(nn.Module):
     # Multi-scale discriminator architecture
@@ -154,7 +86,7 @@ class MsImageDis(nn.Module):
 # Generator
 ##################################################################################
 
-class AdaINGen(nn.Module):
+class networks2(nn.Module):
     # AdaIN auto-encoder architecture
     def __init__(self, input_dim, params):
         super(AdaINGen, self).__init__()
@@ -213,110 +145,6 @@ class AdaINGen(nn.Module):
             if m.__class__.__name__ == "AdaptiveInstanceNorm2d":
                 num_adain_params += 2*m.num_features
         return num_adain_params
-class AdaINGenHr(nn.Module):
-    # AdaIN auto-encoder architecture
-    def __init__(self, input_dim, params):
-        super(AdaINGenHr, self).__init__()
-        dim = params['dim']
-        style_dim = params['style_dim']
-        n_downsample = params['n_downsample']
-        n_res = params['n_res']
-        activ = params['activ']
-        pad_type = params['pad_type']
-        mlp_dim = params['mlp_dim']
-
-        # style encoder
-        self.enc_style = StyleEncoder(4, input_dim, dim, style_dim, norm='none', activ=activ, pad_type=pad_type)
-
-        # content encoder
-        self.enc_content = ContentEncoder(n_downsample, n_res, input_dim, dim, 'in', activ, pad_type=pad_type)
-        self.dec = Decoder(n_downsample, n_res, self.enc_content.output_dim, input_dim, res_norm='adain', activ=activ, pad_type=pad_type)
-
-        # MLP to generate AdaIN parameters
-        self.mlp = MLP(style_dim, self.get_num_adain_params(self.dec), mlp_dim, 3, norm='none', activ=activ)
-
-        self.dec2 = Decoder(n_downsample, n_res, self.enc_content.output_dim, input_dim, res_norm='adain', activ=activ, pad_type=pad_type)
-        # MLP to generate AdaIN parameters
-        self.mlp2 = MLP(style_dim, self.get_num_adain_params(self.dec), mlp_dim, 3, norm='none', activ=activ)
-
-        self.dec3 = Decoder(n_downsample, n_res, self.enc_content.output_dim, input_dim, res_norm='adain', activ=activ, pad_type=pad_type)
-        # MLP to generate AdaIN parameters
-        self.mlp3 = MLP(style_dim, self.get_num_adain_params(self.dec), mlp_dim, 3, norm='none', activ=activ)
-
-        self.dec4 = Decoder(n_downsample, n_res, self.enc_content.output_dim, input_dim, res_norm='adain', activ=activ, pad_type=pad_type)
-        # MLP to generate AdaIN parameters
-        self.mlp4 = MLP(style_dim, self.get_num_adain_params(self.dec), mlp_dim, 3, norm='none', activ=activ)
-
-        self.dec5 = Decoder(n_downsample, n_res, self.enc_content.output_dim, input_dim, res_norm='adain', activ=activ, pad_type=pad_type)
-        # MLP to generate AdaIN parameters
-        self.mlp5 = MLP(style_dim, self.get_num_adain_params(self.dec), mlp_dim, 3, norm='none', activ=activ)
-
-        self.dec6 = Decoder(n_downsample, n_res, self.enc_content.output_dim, input_dim, res_norm='adain', activ=activ, pad_type=pad_type)
-        # MLP to generate AdaIN parameters
-        self.mlp6 = MLP(style_dim, self.get_num_adain_params(self.dec), mlp_dim, 3, norm='none', activ=activ)
-
-    def forward(self, images):
-        # reconstruct an image
-        content, style_fake = self.encode(images)
-        images_recon = self.decode(content, style_fake)
-        return images_recon
-
-    def encode(self, images):
-        # encode an image to its content and style codes
-        style_fake = self.enc_style(images)
-        content = self.enc_content(images)
-        return content, style_fake
-
-    def decode(self, content, style):
-        # decode content and style codes to an image
-        adain_params = self.mlp(style)
-        self.assign_adain_params(adain_params, self.dec)
-        images = self.dec(content)
-
-        adain_params2 = self.mlp2(style)
-        self.assign_adain_params(adain_params2, self.dec2)
-        images2 = self.dec2(content)
-
-        adain_params3 = self.mlp3(style)
-        self.assign_adain_params(adain_params3, self.dec3)
-        images3 = self.dec3(content)
-
-        adain_params4 = self.mlp4(style)
-        self.assign_adain_params(adain_params4, self.dec4)
-        images4 = self.dec4(content)
-
-        adain_params5 = self.mlp5(style)
-        self.assign_adain_params(adain_params5, self.dec5)
-        images5 = self.dec5(content)
-
-        adain_params6 = self.mlp6(style)
-        self.assign_adain_params(adain_params6, self.dec6)
-        images6 = self.dec6(content)
-
-        res=torch.cat([images,images2,images3,images4,images5,images6],dim=1)
-        #res=torch.cat([images,images2,images3],dim=1)
-        out_images=torch.mean(res,axis=1,keepdim=True)
-        return out_images,res
-
-    def assign_adain_params(self, adain_params, model):
-        # assign the adain_params to the AdaIN layers in model
-        for m in model.modules():
-            if m.__class__.__name__ == "AdaptiveInstanceNorm2d":
-                mean = adain_params[:, :m.num_features]
-                std = adain_params[:, m.num_features:2*m.num_features]
-                m.bias = mean.contiguous().view(-1)
-                m.weight = std.contiguous().view(-1)
-                if adain_params.size(1) > 2*m.num_features:
-                    adain_params = adain_params[:, 2*m.num_features:]
-
-    def get_num_adain_params(self, model):
-        # return the number of AdaIN parameters needed by the model
-        num_adain_params = 0
-        for m in model.modules():
-            if m.__class__.__name__ == "AdaptiveInstanceNorm2d":
-                num_adain_params += 2*m.num_features
-        return num_adain_params
-
 
 class VAEGen(nn.Module):
     # VAE architecture
@@ -329,7 +157,7 @@ class VAEGen(nn.Module):
         pad_type = params['pad_type']
 
         # content encoder
-        self.enc = ContentEncoder(n_downsample, n_res, input_dim, dim, 'in', activ, pad_type=pad_type)
+        self.enc = Encoder(n_downsample, n_res, input_dim, dim, 'in', activ, pad_type=pad_type)
         self.dec = Decoder(n_downsample, n_res, self.enc.output_dim, input_dim, res_norm='in', activ=activ, pad_type=pad_type)
 
     def forward(self, images):
@@ -355,26 +183,7 @@ class VAEGen(nn.Module):
 ##################################################################################
 # Encoder and Decoders
 ##################################################################################
-
-class StyleEncoder(nn.Module):
-    def __init__(self, n_downsample, input_dim, dim, style_dim, norm, activ, pad_type):
-        super(StyleEncoder, self).__init__()
-        self.model = []
-        self.model += [Conv2dBlock(input_dim, dim, 7, 1, 3, norm=norm, activation=activ, pad_type=pad_type)]
-        for i in range(2):
-            self.model += [Conv2dBlock(dim, 2 * dim, 4, 2, 1, norm=norm, activation=activ, pad_type=pad_type)]
-            dim *= 2
-        for i in range(n_downsample - 2):
-            self.model += [Conv2dBlock(dim, dim, 4, 2, 1, norm=norm, activation=activ, pad_type=pad_type)]
-        self.model += [nn.AdaptiveAvgPool2d(1)] # global average pooling
-        self.model += [nn.Conv2d(dim, style_dim, 1, 1, 0)]
-        self.model = nn.Sequential(*self.model)
-        self.output_dim = dim
-
-    def forward(self, x):
-        return self.model(x)
-
-class ContentEncoder(nn.Module):
+class Encoder(nn.Module):
     def __init__(self, n_downsample, n_res, input_dim, dim, norm, activ, pad_type):
         super(ContentEncoder, self).__init__()
         self.model = []
