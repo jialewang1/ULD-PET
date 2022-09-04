d97e705457f75c872f6a50f7bb5bf5330f7d6f66
jialewang1 <108407828+jialewang1@users.noreply.github.com>
2022-09-04 21:45:45 +0800

    Create trainer.py

---
+++ trainer.py
@@ -0,0 +1,100 @@
+"""
+Copyright (C) 2017 NVIDIA Corporation.  All rights reserved.
+Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
+"""
+from networks import MsImageDis,network1
+from utils import weights_init, get_model_list, vgg_preprocess, load_vgg16, get_scheduler
+from torch.autograd import Variable
+import torch
+import torch.nn as nn
+import os
+ class MUNIT_Trainer(nn.Module):
+    def __init__(self, hyperparameters):
+        super(MUNIT_Trainer, self).__init__()
+        lr = hyperparameters['lr']
+        # Initiate the networks
+        self.gen_a = network1(
+           in_chans=1,
+           out_chans=1,
+           chans=128,
+           num_pool_layers=4,
+           drop_prob=0.2,
+        )
+        self.dis_a = MsImageDis(hyperparameters['input_dim_a'], hyperparameters['dis'])  # discriminator for domain a
+        self.instancenorm = nn.InstanceNorm2d(512, affine=False)
+        # Setup the optimizers
+        beta1 = hyperparameters['beta1']
+        beta2 = hyperparameters['beta2']
+        dis_params = list(self.dis_a.parameters()) 
+        gen_params = list(self.gen_a.parameters()) 
+        self.dis_opt = torch.optim.Adam([p for p in dis_params if p.requires_grad],
+                                        lr=lr, betas=(beta1, beta2), weight_decay=hyperparameters['weight_decay'])
+        self.gen_opt = torch.optim.Adam([p for p in gen_params if p.requires_grad],
+                                        lr=lr, betas=(beta1, beta2), weight_decay=hyperparameters['weight_decay'])
+        self.dis_scheduler = get_scheduler(self.dis_opt, hyperparameters)
+        self.gen_scheduler = get_scheduler(self.gen_opt, hyperparameters)
+
+        # Network weight initialization
+        self.apply(weights_init(hyperparameters['init']))
+        self.dis_a.apply(weights_init('gaussian'))
+
+
+        # Load VGG model if needed
+        if 'vgg_w' in hyperparameters.keys() and hyperparameters['vgg_w'] > 0:
+            self.vgg = load_vgg16(hyperparameters['vgg_model_path'] + '/models')
+            self.vgg.eval()
+            for param in self.vgg.parameters():
+                param.requires_grad = False
+     def update_learning_rate(self):
+        if self.dis_scheduler is not None:
+            self.dis_scheduler.step()
+        if self.gen_scheduler is not None:
+            self.gen_scheduler.step()
+
+    def resume(self, checkpoint_dir, hyperparameters):
+        # del_dict=[]
+        # last_model_name = get_model_list(checkpoint_dir, "gen")
+        # iterations = int(last_model_name[-11:-3])
+        # state_dict = torch.load(last_model_name)
+        # model_dict = self.gen_a.state_dict()
+        # pretrained_dict = {k: v for k, v in state_dict['a'].items() if k not in del_dict}
+        # model_dict.update(pretrained_dict)
+        # self.gen_a.load_state_dict(model_dict)
+        # print('Resume from iteration %d' % iterations)
+        # iterations=0
+        # Load generators
+        last_model_name = get_model_list(checkpoint_dir, "gen")
+        state_dict = torch.load(last_model_name)
+        self.gen_a.load_state_dict(state_dict['a'])
+        iterations = int(last_model_name[-11:-3])
+        # Load discriminators
+        last_model_name = get_model_list(checkpoint_dir, "dis")
+        state_dict = torch.load(last_model_name)
+        self.dis_a.load_state_dict(state_dict['a'])
+        #Load optimizers
+        state_dict = torch.load(os.path.join(checkpoint_dir, 'optimizer.pt'))
+        self.dis_opt.load_state_dict(state_dict['dis'])
+        self.gen_opt.load_state_dict(state_dict['gen'])
+        print('Resume from iteration %d' % iterations)
+        # iterations=0
+        # ## Reinitilize schedulers
+        # self.dis_opt.param_groups[0]['lr']=hyperparameters['lr']
+        # self.gen_opt.param_groups[0]['lr']=hyperparameters['lr']
+        # self.dis_scheduler = get_scheduler(self.dis_opt, hyperparameters)
+        # self.gen_scheduler = get_scheduler(self.gen_opt, hyperparameters)
+
+        self.dis_scheduler = get_scheduler(self.dis_opt, hyperparameters, iterations)
+        self.gen_scheduler = get_scheduler(self.gen_opt, hyperparameters, iterations)
+        
+        return iterations
+
+    def save(self, snapshot_dir, iterations):
+        # Save generators, discriminators, and optimizers
+        gen_name = os.path.join(snapshot_dir, 'gen_%08d.pt' % (iterations + 1))
+        dis_name = os.path.join(snapshot_dir, 'dis_%08d.pt' % (iterations + 1))
+        opt_name = os.path.join(snapshot_dir, 'optimizer.pt')
+        torch.save({'a': self.gen_a.state_dict()}, gen_name)
+        torch.save({'a': self.dis_a.state_dict()}, dis_name)
+        torch.save({'gen': self.gen_opt.state_dict(), 'dis': self.dis_opt.state_dict()}, opt_name)
+
+
